dd856f1339963f74ace464a5fd930ea54190a9ee
s0163451 <90443233+s0163451@users.noreply.github.com>
2021-11-19 01:08:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+
+let isOpen = true;
+let isClick = false;
+let hash = window.location.hash;
+
+$(function () {
+    setInterval(function () {
+        if (window.location.hash !== hash && isClick) {
+            hash = window.location.hash;
+            console.log("hash has been changed!");
+            if (isOpen) {
+                closePopup();
+                isOpen = false;
+            } else {
+                openPopup();
+                isOpen = true;
+            }
+        }
+    }, 100);
+
+    $("#openBut").click(function () {
+        isClick = false;
+        console.log("click on button");
+        openPopup();
+    });
+
+    function openPopup(){
+        console.log("open!");
+        if (!isClick) {
+            let stateObj = {task8: "#form"};
+            history.pushState(stateObj, null, "#form");
+            isClick = true;
+            isOpen = true;
+        }
+        $(".hidden").css("background-color", "rgba(0, 0, 0, 0.31)").show();
+        hash = window.location.hash;
+    }
+
+    function closePopup(){
+        console.log("close!");
+        $(".hidden").css("background-color", "rgba(255,255,255)").hide();
+    }
+
+    $("#form").submit(function(e){
+        e.preventDefault();
+        let formData = new FormData(document.querySelector('form'));
+        let object = {};
+        formData.forEach((value, key) => object[key] = value);
+        let jsonString = JSON.stringify(object);
+        let href = "https://formcarry.com/s/uTfUjzfRQbm";
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: JSON.parse(jsonString),
+            success: function(response){
+                if(response.status === "success"){
+                    alert("Data was successfully sent!");
+                    document.getElementById('form').reset();
+                }else{
+                    alert("ERROR: " + response.message);
+                }
+            }
+        });
+    });
+
+    localStorage.setItem("name", document.getElementById("name").value);
+    localStorage.setItem("email", document.getElementById("email").value);
+    localStorage.setItem("comment", document.getElementById("comment").value);
+});
